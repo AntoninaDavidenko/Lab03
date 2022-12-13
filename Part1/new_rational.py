97cf2d73bb4b6ac127ad73cc3634d04c0cd09d0c
AntoninaDavidenko <44519188+AntoninaDavidenko@users.noreply.github.com>
2022-12-13 21:29:48 +0200

    Update new_rational.py

--- Part1/new_rational.py
+++ Part1/new_rational.py
@@ -21,8 +21,15 @@ class Rational:
     def standard_form(self):
         print(f"{self.__numerator}/{self.__denominator}")
 
+    def standard_form_second(self):
+        print(f"{self.__second_numerator}/{self.__second_denominator}")
+
     def floating_point(self):
         result = self.__numerator / self.__denominator
+        print(int(result))
+
+    def floating_point_second(self):
+        result = self.__second_numerator / self.__second_denominator
         print(int(result))
 
     def add(self):
@@ -49,13 +56,23 @@ class Rational:
         result = numerator / denominator
         print(int(result))
 
+    def comp(self):
+        temp = self.__numerator * self.__second_denominator - self.__second_numerator * self.__denominator
+        if temp > 0:
+            print(f"{self.__numerator}/{self.__denominator} is bigger")
+        elif temp == 0:
+            print("Rational numbers are equal")
+        elif temp < 0:
+            print(f"{self.__second_numerator}/{self.__second_denominator} is bigger")
+
 
 pr = Rational()
-pr.set_numerator(3)
-pr.set_denominator(4)
+pr.set_numerator(4)
+pr.set_denominator(5)
 pr.set_second_numerator(2)
-pr.set_second_denominator(4)
+pr.set_second_denominator(3)
 pr.add()
 pr.sub()
 pr.mul()
 pr.div()
+pr.comp()
