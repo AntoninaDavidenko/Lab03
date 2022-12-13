1f051f60659ab8945fc85550e189ece92bb27f92
AntoninaDavidenko <44519188+AntoninaDavidenko@users.noreply.github.com>
2022-12-13 20:19:40 +0200

    Update new_rational.py

--- Part1/new_rational.py
+++ Part1/new_rational.py
@@ -1,51 +1,51 @@
 class Rational:
 
     def __init__(self):
-        self.numerator = 1
-        self.denominator = 2
-        self.second_numerator = 1
-        self.second_denominator = 2
+        self.__numerator = 1
+        self.__denominator = 2
+        self.__second_numerator = 1
+        self.__second_denominator = 2
 
     def set_numerator(self, numerator):
-        self.numerator = numerator
+        self.__numerator = numerator
 
     def set_denominator(self, denominator):
-        self.denominator = denominator
+        self.__denominator = denominator
 
     def set_second_numerator(self, second_numerator):
-        self.second_numerator = second_numerator
+        self.__second_numerator = second_numerator
 
     def set_second_denominator(self, second_denominator):
-        self.second_denominator = second_denominator
+        self.__second_denominator = second_denominator
 
     def standard_form(self):
-        print(f"{self.numerator}/{self.denominator}")
+        print(f"{self.__numerator}/{self.__denominator}")
 
     def floating_point(self):
-        result = self.numerator / self.denominator
+        result = self.__numerator / self.__denominator
         print(int(result))
 
     def add(self):
-        numerator = self.numerator * self.second_denominator + self.second_numerator * self.denominator
-        denominator = self.denominator * self.second_denominator
+        numerator = self.__numerator * self.__second_denominator + self.__second_numerator * self.__denominator
+        denominator = self.__denominator * self.__second_denominator
         result = numerator / denominator
         print(int(result))
 
     def sub(self):
-        numerator = self.numerator * self.second_denominator - self.second_numerator * self.denominator
-        denominator = self.denominator * self.second_denominator
+        numerator = self.__numerator * self.__second_denominator - self.__second_numerator * self.__denominator
+        denominator = self.__denominator * self.__second_denominator
         result = numerator / denominator
         print(int(result))
 
     def mul(self):
-        numerator = self.numerator * self.second_numerator
-        denominator = self.denominator * self.second_denominator
+        numerator = self.__numerator * self.__second_numerator
+        denominator = self.__denominator * self.__second_denominator
         result = numerator / denominator
         print(int(result))
 
     def div(self):
-        numerator = self.numerator * self.second_denominator
-        denominator = self.denominator * self.second_numerator
+        numerator = self.__numerator * self.__second_denominator
+        denominator = self.__denominator * self.__second_numerator
         result = numerator / denominator
         print(int(result))
 
